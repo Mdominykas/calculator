328786ec52fe44974cfbde79857df98c3eb57b80
Mdominykas <marmadominykas@gmail.com>
2019-05-27 20:17:34 +0300

    pirmas filas

---
+++ calculator.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct veiksmai
+{
+	string eilute;
+	vector<double> skaiciai;
+	vector<int> tvarka;
+	vector<int> gylis;
+	veiksmai()
+}
+
+int main()
+{
+	string eilute;
+	cin >> eilute;
+	return 0;
+}
